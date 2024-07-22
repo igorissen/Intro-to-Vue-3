d6defb7e1f5536a534fa4a81824e193038cff691
Ismaël Gorissen <igo@keystone-solutions.be>
2024-07-22 11:47:48 +0200

    feat: complete exercise

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes: [
+                { id: 3334, label: 'xs' },
+                { id: 3335, label: 's' },
+                { id: 3336, label: 'm' },
+                { id: 3337, label: 'l' },
+                { id: 3338, label: 'xl' }
+            ]
         }
     }
 })
